d0e0625365ac57786ccb8e85d82d65e9c735d577
tigers2020 <tigers2020@hotmail.com>
2025-07-21 13:49:34 -0400

    Clean up project structure for new implementation

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-# AnimeSorter 패키지 초기화
-# 핵심 모듈(추후 구현) import 위치
-from .core import *
- 
--- src/core/cli.py
+++
@@ -1,9 +0,0 @@
-# cli.py: AnimeSorter CLI 엔트리포인트 (스켈레톤)
-
-def main():
-    """AnimeSorter CLI 메인 함수"""
-    # TODO: argparse 등 구현
-    pass
-
-if __name__ == "__main__":
-    main() 
